64df015ccd3efd94f0f136b5b752da17e4c37297
bgergely <30804316+ggbalaazs@users.noreply.github.com>
2018-06-05 21:07:31 +0200

    Light classifier without graph

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -139,21 +139,14 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
 
-        # FIXME Temporary For testing against ground truth, delete this line and uncomment the commented section below when the classifier is ready
-        return light.state
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        # if(not self.has_image):
-
-            # self.prev_light_loc = None
-
-            # return False
-
-        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-
-        # #Get classification
-
-        # return self.light_classifier.get_classification(cv_image)
+        #Get classification
+        return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
